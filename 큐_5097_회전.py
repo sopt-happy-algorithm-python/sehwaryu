f4d6ab884296338465e03a5f4c9b4dcdf4e2b3f3
Sehwa <sehwa_ryu@berkeley.edu>
2020-08-15 22:05:38 +0900

    Delete 큐_5097_회전.py

--- 큐_5097_회전.py
+++
@@ -1,9 +0,0 @@
-T = int(input())
-
-for i in range(1, T+1):
-    N, M = map(int, input().split())
-    lst = list(map(int, input().split()))
-    for j in range(M):
-        a = lst.pop(0)
-        lst.append(a)
-    print (f'#{i} {lst[0]}')
