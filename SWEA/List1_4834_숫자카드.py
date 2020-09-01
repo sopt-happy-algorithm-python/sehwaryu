7df3fe4ca462bf0486bfc2da8808454425343239
Sehwa <sehwa_ryu@berkeley.edu>
2020-09-01 16:24:13 +0900

    Update List1_4834_숫자카드.py

--- SWEA/List1_4834_숫자카드.py
+++ SWEA/List1_4834_숫자카드.py
@@ -27,3 +27,26 @@ for j in range(1, T+1):
             max = cnt[i]
     print("#%d %d %d" %(j, ans, cnt[ans]))
                 
+
+/**각 번호의 개수를 저장할 리스트를 만든후에 나온 카드의 번호에 해당하는 인덱스에 접근하여 갯수를 하나씩 늘린뒤에
+뒤부터 검사하여 제일 많이나온 카드를 확인.**/
+
+length = int(input())
+for l in range(1, length+1):
+    size = input()
+    temp = list(map(int, list(input())))
+    #각 숫자 카운팅하는 변수
+    numcheck = [0]*10
+    #각 숫자 카운팅하기
+    for t in temp:  
+        numcheck[t] += 1
+    #숫자와 횟수
+    index = 0
+    maxnum = 0
+    #반복문
+    for i in range(9, -1, -1):
+        if numcheck[i] > maxnum :
+            maxnum = numcheck[i]
+            index = i
+    #출력
+    print("#{} {} {}".format(l, index, maxnum))
